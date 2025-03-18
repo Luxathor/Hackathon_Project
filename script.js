1d71a7c5415ec071fd6f0d61ab88f5f16a9c9c85
Bhumika <dbhumika829@gmail.com>
2025-03-18 14:52:18 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openModal() {
+    document.getElementById("edit-modal").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("edit-modal").style.display = "none";
+}
+
